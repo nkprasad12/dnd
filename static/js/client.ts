140ef21f6c65fd74c08dfdba0d94274c52079740
Nitin Prasad <nkprasa@gmail.com>
2021-02-11 01:22:49 -0600

    Organize code into classes

--- static/js/client.ts
+++ static/js/client.ts
@@ -1,7 +1,3 @@
-var backgroundCanvas = <HTMLCanvasElement>document.getElementById("backgroundCanvas");
-var fogOfWarCanvas = <HTMLCanvasElement>document.getElementById("fogOfWarCanvas");
-var gridCanvas = <HTMLCanvasElement>document.getElementById("gridCanvas");
-
 // Usage:
 // var client = new HttpClient();
 // client.get('http://blah/request', function (response: string) {
@@ -20,8 +16,135 @@ var HttpClient = function () {
     }
 }
 
-function drawTile(x: number, y: number, size: number, color: string, canvas: { getContext: (arg0: string) => any; }) {
-    var ctx = canvas.getContext("2d");
+/** Represents the main game board. */
+class GameBoard {
+
+    tileSize: number;
+    width: number;
+    height: number;
+
+    backgroundCanvas: HTMLCanvasElement;
+    fogOfWarCanvas: HTMLCanvasElement;
+    tokenCanvas: HTMLCanvasElement;
+    gridCanvas: HTMLCanvasElement;
+    topCanvas: HTMLCanvasElement;
+
+    allCanvases: Array<HTMLCanvasElement>;
+
+    constructor(tileSize: number) {
+        this.backgroundCanvas = <HTMLCanvasElement>document.getElementById("backgroundCanvas");
+        this.fogOfWarCanvas = <HTMLCanvasElement>document.getElementById("fogOfWarCanvas");
+        this.tokenCanvas = <HTMLCanvasElement>document.getElementById("tokenCanvas");
+        this.gridCanvas = <HTMLCanvasElement>document.getElementById("gridCanvas");
+        this.topCanvas = <HTMLCanvasElement>document.getElementById("topCanvas");
+
+        this.allCanvases = [
+            this.backgroundCanvas, this.fogOfWarCanvas, this.tokenCanvas, this.gridCanvas, this.topCanvas]
+
+        this.tileSize = Math.round(tileSize)
+        if (this.tileSize != tileSize) {
+            console.log("Rounded input tileSize to " + this.tileSize)
+        }
+    }
+
+    setBackground(source: string) {
+        let image = new Image();
+        image.src = source
+        image.onload = (event) => {
+            let loadedImage = <CanvasImageSource>event.currentTarget
+            this.width = <number>loadedImage.width
+            this.height = <number>loadedImage.height
+            console.log('Loaded image: ' + this.width + 'x' + this.height)
+
+            for (var canvas of this.allCanvases) {
+                console.log(canvas)
+                canvas.width = this.width
+                canvas.height = this.height
+                getContext(canvas).clearRect(0, 0, this.width, this.height)
+            }
+            getContext(this.backgroundCanvas).drawImage(loadedImage, 0, 0)
+            this.drawGrid(this.gridCanvas)
+            this.topCanvas.addEventListener(
+                'mousedown',
+                (e) => {
+                    this.onMouseClick(e)
+                })
+        }
+    }
+
+    drawGrid(canvas: HTMLCanvasElement) {
+        const { width, height } = canvas.getBoundingClientRect();
+
+        var hSquares = Math.ceil(height / this.tileSize);
+        var wSquares = Math.ceil(width / this.tileSize);
+
+        var i: number;
+        var j: number;
+
+        const gridColor: string = "rgba(255, 255, 255, 0.1)";
+        for (i = 0; i < hSquares; i++) {
+            for (j = 0; j < wSquares; j++) {
+                drawCanvasTile(j * this.tileSize, i * this.tileSize, this.tileSize, gridColor, this.gridCanvas);
+            }
+        }
+    }
+
+    outOfBounds(point: Point) {
+        const { width, height } = this.topCanvas.getBoundingClientRect();
+        if (point.x < 0 || point.y < 0) {
+            return true
+        } else if (point.x >= width) {
+            return true
+        } else if (point.y >= height) {
+            return true
+        }
+        return false
+    }
+
+    handleMouseClick(point: Point) {
+        if (this.outOfBounds(point)) {
+            return
+        }
+        const xStart = Math.floor(point.x / this.tileSize) * this.tileSize
+        const yStart = Math.floor(point.y / this.tileSize) * this.tileSize
+        fillCanvasTile(xStart, yStart, this.tileSize, "rgba(0, 0, 0, 1.0)", this.fogOfWarCanvas)
+    }
+
+    onMouseClick(event: MouseEvent) {
+        console.log('onMouseClick')
+
+        const rect = this.topCanvas.getBoundingClientRect()
+        const xVal = event.clientX - rect.left
+        const yVal = event.clientY - rect.top
+        console.log("Mouse click canvas coordinates: (x: " + xVal + ", y: " + yVal + ")")
+        this.handleMouseClick({ x: xVal, y: yVal })
+    }
+
+}
+
+/** Represents a tile in the game board. */
+class Tile {
+
+    size: number;
+    startX: number;
+    startY: number;
+
+    fogOfWarCanvas: HTMLCanvasElement;
+
+    constructor(size: number, startX: number, startY: number, fogOfWarCanvas: HTMLCanvasElement) {
+        this.size = size;
+        this.startX = startX;
+        this.startY = startY;
+        this.fogOfWarCanvas = fogOfWarCanvas;
+    }
+}
+
+function getContext(canvas: HTMLCanvasElement) {
+    return canvas.getContext("2d");
+}
+
+function drawCanvasTile(x: number, y: number, size: number, color: string, canvas: HTMLCanvasElement) {
+    var ctx = getContext(canvas)
 
     ctx.beginPath();
     ctx.rect(x, y, size, size);
@@ -30,8 +153,8 @@ function drawTile(x: number, y: number, size: number, color: string, canvas: { g
     ctx.closePath();
 }
 
-function fillTile(x: number, y: number, size: number, color: string, canvas: { getContext: (arg0: string) => any; }) {
-    var ctx = canvas.getContext("2d");
+function fillCanvasTile(x: number, y: number, size: number, color: string, canvas: HTMLCanvasElement) {
+    var ctx = getContext(canvas)
 
     ctx.beginPath();
     ctx.rect(x, y, size, size);
@@ -40,91 +163,11 @@ function fillTile(x: number, y: number, size: number, color: string, canvas: { g
     ctx.closePath();
 }
 
-function outOfBounds(point: { x: number; y: number; }, canvas: { getBoundingClientRect: () => { width: any; height: any; }; }) {
-    const { width, height } = canvas.getBoundingClientRect();
-    if (point.x < 0 || point.y < 0) {
-        console.log("Negative indexed point")
-        return true
-    } else if (point.x >= width) {
-        console.log("OOB X")
-        return true
-    } else if (point.y >= height) {
-        console.log("OOB Y")
-        return true
-    }
-    return false
+interface Point {
+    x: number;
+    y: number;
 }
 
-function handleMouseClick(point: { x: number; y: number; }, canvas: any) {
-    if (outOfBounds(point, canvas)) {
-        return
-    }
-    const xStart = Math.floor(point.x / 50) * 50
-    const yStart = Math.floor(point.y / 50) * 50
-    console.log("xStart: " + xStart + ", yStart: " + yStart)
-    fillTile(xStart, yStart, 50, "rgba(0, 0, 0, 0.8)", canvas)
-}
-
-function onMouseClick(canvas: HTMLElement, event: MouseEvent) {
-    const rect = canvas.getBoundingClientRect()
-    const xVal = event.clientX - rect.left
-    const yVal = event.clientY - rect.top
-    console.log("x: " + xVal + " y: " + yVal)
-    handleMouseClick({ x: xVal, y: yVal }, canvas)
-}
-
-gridCanvas.addEventListener('mousedown', function (e) {
-    onMouseClick(fogOfWarCanvas, e)
-})
-
-function drawGrid(canvas: HTMLCanvasElement) {
-    const { width, height } = canvas.getBoundingClientRect();
-
-    var tileSize = 50;
-    var hSquares = Math.ceil(height / tileSize);
-    var wSquares = Math.ceil(width / tileSize);
-
-    var i: number;
-    var j: number;
-
-    for (i = 0; i < hSquares; i++) {
-        for (j = 0; j < wSquares; j++) {
-            var color = "rgba(255, 255, 255, 0.1)"
-            drawTile(j * 50, i * 50, 50, color, canvas)
-        }
-    }
-}
-
-function loadCanvasBackground() {
-    let image = new Image();
-    image.src = 'http://localhost:5000/retrieve_image/Screenshot_from_2020-12-18_14-11-08.png'
-    image.onload = function (event) {
-        let loadedImage = <CanvasImageSource> event.currentTarget
-        const width = <number> loadedImage.width
-        const height = <number> loadedImage.height
-        console.log(width + 'x' + height)
-
-        backgroundCanvas.width = width
-        backgroundCanvas.height = height
-        var backgroundContext = backgroundCanvas.getContext("2d");
-        backgroundContext.drawImage(loadedImage, 0, 0)
-
-        fogOfWarCanvas.width = width
-        fogOfWarCanvas.height = height
-        // var fogOfWarContext = fogOfWarCanvas.getContext("2d");
-        // fogOfWarContext.fillStyle = "rgba(0, 0, 0, 0.3)"
-        // fogOfWarContext.fillRect(0, 0, width, height)        
-        // // fogOfWarContext.clearRect(0, 0, width, height)        
-
-        gridCanvas.width = width
-        gridCanvas.height = height
-        // var gridContext = gridCanvas.getContext("2d");
-        // gridContext.fillStyle = "rgba(0, 0, 0, 0.3)"
-        // gridContext.fillRect(0, 0, width, height)        
-        // // gridContext.clearRect(0, 0, width, height)  
-
-        drawGrid(gridCanvas)
-    }
-}
-
-loadCanvasBackground()
+const backgroundUrl = 'http://localhost:5000/retrieve_image/Screenshot_from_2020-12-18_14-11-08.png'
+var board = new GameBoard(60)
+board.setBackground(backgroundUrl)
