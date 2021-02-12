1b07e4080c3b15e17255d0d590ca1d5c3e353f96
Nitin Prasad <nkprasa@gmail.com>
2021-02-11 22:18:09 -0600

    Change upload to use two spaces

--- static/js/upload.ts
+++ static/js/upload.ts
@@ -1,28 +1,28 @@
 const handleImageUpload = (event) => {
-    const files = event.target.files
-    const formData = new FormData()
-    formData.append('file', files[0])
+  const files = event.target.files
+  const formData = new FormData()
+  formData.append('file', files[0])
 
-    fetch('http://127.0.0.1:5000/uploadImage', {
-        method: 'POST',
-        body: formData
+  fetch('http://127.0.0.1:5000/uploadImage', {
+    method: 'POST',
+    body: formData
+  })
+    .then(response => {
+      console.log(response)
+      return response.json()
     })
-        .then(response => {
-            console.log(response)
-            return response.json()
-        })
-        .then(data => {
-            console.log('Data: ' + data)
-            console.log('Path: ' + data.path)
-        })
-        .catch(error => {
-            console.error('Error: ' + error)
-        })
+    .then(data => {
+      console.log('Data: ' + data)
+      console.log('Path: ' + data.path)
+    })
+    .catch(error => {
+      console.error('Error: ' + error)
+    })
 }
 
 var upload = document.querySelector('#fileUpload')
 console.log(upload)
 upload.addEventListener('change', event => {
-    console.log('Got image upload request')
-    handleImageUpload(event)
+  console.log('Got image upload request')
+  handleImageUpload(event)
 })
