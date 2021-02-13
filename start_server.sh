4a6d83004b2e40596609704b8ce4e1879c5cdb42
Nitin Prasad <nkprasa@gmail.com>
2021-02-13 10:05:45 -0600

    Add comment for how to add public server

---
+++ start_server.sh
@@ -0,0 +1,7 @@
+export FLASK_APP=dnd
+export FLASK_ENV=development
+tsc dnd/static/js/client.ts
+tsc dnd/static/js/upload.sh.ts
+flask run
+# This will cause it to be pubically available
+# flask run --host=0.0.0.0
