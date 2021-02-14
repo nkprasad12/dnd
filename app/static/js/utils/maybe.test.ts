56001a5c032ccf62cf846b7f10bfcabe7b470791
Nitin Prasad <nkprasa@gmail.com>
2021-02-13 22:09:00 -0600

    Clear Maybe class and set up unit testing

---
+++ app/static/js/utils/maybe.test.ts
@@ -0,0 +1,24 @@
+import { hasUncaughtExceptionCaptureCallback } from "process";
+import { Maybe } from "./maybe"
+
+class TestObject {
+    foo: number;
+
+    constructor(foo: number) {
+        this.foo = foo;
+    }
+}
+
+test('Absent Maybe returns present false', () => {
+    expect(Maybe.absent().present()).toBe(false);
+});
+
+test('Present Maybe returns present true', () => {
+    let testObject = new TestObject(5);
+    expect(Maybe.of(testObject).present()).toBe(true);
+});
+
+test('Present Maybe returns original', () => {
+    let testObject = new TestObject(5);
+    expect(Maybe.of(testObject).get()).toBe(testObject);
+});
