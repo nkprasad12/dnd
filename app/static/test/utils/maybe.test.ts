5df068fe619aa84f1cd8cc51abafd84d54ede4b2
Nitin Prasad <nkprasa@gmail.com>
2021-02-14 12:30:15 -0600

    Wrap test/expect to hide vscode errors, move test

---
+++ app/static/test/utils/maybe.test.ts
@@ -0,0 +1,26 @@
+// @ts-ignore
+import { test_, expect_ } from "../test_util.ts"
+
+import { Maybe } from "../../src/utils/maybe"
+
+class TestObject {
+    foo: number;
+
+    constructor(foo: number) {
+        this.foo = foo;
+    }
+}
+
+test_('Absent Maybe returns present false', () => {
+    expect_(Maybe.absent().present()).toBe(false);
+});
+
+test_('Present Maybe returns present true', () => {
+    let testObject = new TestObject(5);
+    expect_(Maybe.of(testObject).present()).toBe(true);
+});
+
+test_('Present Maybe returns original', () => {
+    let testObject = new TestObject(5);
+    expect_(Maybe.of(testObject).get()).toBe(testObject);
+});
