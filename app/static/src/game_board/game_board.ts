eaf00fcbcf1c18021a4098d11cc867d45e6a232c
Nitin Prasad <nkprasa@gmail.com>
2021-02-20 19:16:57 -0600

    Load active board in game board

--- app/static/src/game_board/game_board.ts
+++ app/static/src/game_board/game_board.ts
@@ -1,58 +1,19 @@
-// import { GameBoard } from './game_board/game_board'
 import {BoardModel} from '/src/game_board/model/board_model';
-// import { BoardView } from './game_board/view/board_view'
-import {LoadedImage, loadImages} from '/src/utils/image_utils';
 import {connectTo} from '/src/server/socket_connection';
 import {GameBoard} from '/src/game_board/controller/game_board';
 import {BoardServer} from '/src/game_board/remote/board_server';
 
-const backgroundUrl = 'http://localhost:5000/retrieve_image/grrrr.jpg';
-const wolfUrl = 'http://localhost:5000/retrieve_image/demon.png';
-const egbertUrl = 'http://localhost:5000/retrieve_image/egbert.png';
+const GAME_HOLDER_STUB = 'canvasHolder';
 
-const canvasHolder = document.getElementById('canvasHolder');
-if (canvasHolder == null) {
-  throw new Error('canvasHolder is null! Can not display board');
+async function loadActiveBoard(): Promise<GameBoard> {
+  const server = await serverPromise;
+  const boardId = await server.requestActiveBoardId();
+  const remoteModel = await server.requestBoard(boardId);
+  const model = await BoardModel.createFromRemote(remoteModel);
+  return new GameBoard(GAME_HOLDER_STUB, model, server);
 }
 
-const socketPromise = connectTo('board');
-const imagesPromise = loadImages([backgroundUrl, wolfUrl, egbertUrl]);
+const serverPromise =
+    connectTo('board').then((socket) => new BoardServer(socket));
 
-Promise.all([imagesPromise, socketPromise])
-    .then((inputs) => {
-      const [imageMap, socket] = inputs;
-
-      const modelBuilder =
-          BoardModel.Builder.forNewBoard()
-              .setTileSize(60)
-              .setName('MyTestBoard')
-              .setBackgroundImage(
-                  new LoadedImage(
-                      getOrThrow(imageMap, backgroundUrl),
-                      backgroundUrl));
-      // modelBuilder.addToken(
-      //     TokenModel.create(
-      //         'Wolf',
-      //         new LoadedImage(getOrThrow(imageMap, wolfUrl), wolfUrl),
-      //         1,
-      //         {col: 5, row: 5}, false));
-      // modelBuilder.addToken(
-      //     TokenModel.create(
-      //         'Egbert',
-      //         new LoadedImage(getOrThrow(imageMap, egbertUrl), egbertUrl),
-      //         1,
-      //         {col: 6, row: 6}, false));
-
-      const gameController =
-          new GameBoard(
-              'canvasHolder', modelBuilder.build(), new BoardServer(socket));
-      console.log(gameController);
-    });
-
-function getOrThrow<K, V>(map: Map<K, V>, key: K): V {
-  const value = map.get(key);
-  if (value == undefined) {
-    throw new Error('No value for key: ' + String(key));
-  }
-  return value;
-}
+loadActiveBoard();
