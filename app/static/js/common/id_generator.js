612fa74ba58f262b08848fe0119a3dd4660c9ee3
Nitin Prasad <nkprasa@gmail.com>
2021-02-22 09:25:17 -0600

    Remove javascript

--- app/static/js/common/id_generator.js
+++
@@ -1,6 +0,0 @@
-/** Generates a (hopefully) unique (enough) id. */
-export function getId() {
-    const dateComponent = String(Date.now());
-    const randomComponent = String(Math.floor(Math.random() * 10000));
-    return randomComponent + '-' + dateComponent;
-}
