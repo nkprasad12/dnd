e27a63abc9fa35b27eed69ec5b23049d1ca17736
Nitin Prasad <nkprasa@gmail.com>
2021-03-03 00:17:04 -0600

    Add test for startup scripts

---
+++ src/server/util/startup_scripts.test.ts
@@ -0,0 +1,43 @@
+import fs from 'fs';
+
+import {writeGoogleCredentials} from './startup_scripts';
+
+const EXPECTED_FILE = 'Hasdrubal.txt';
+const ORIGINAL_ENV = process.env;
+
+function removeOutputs() {
+  if (fs.existsSync(EXPECTED_FILE)) {
+    fs.rmSync(EXPECTED_FILE);
+  }
+}
+
+beforeEach(() => {
+  jest.resetModules();
+  process.env = {...ORIGINAL_ENV};
+  removeOutputs();
+});
+
+afterEach(() => {
+  removeOutputs();
+});
+
+test('writeGoogleCredentials without APP_CREDENTIALS throws', () => {
+  process.env.GOOGLE_KEY = 'Hamilcar';
+  expect(() => writeGoogleCredentials()).toThrow();
+});
+
+test('writeGoogleCredentials without GOOGLE_KEY', () => {
+  process.env.GOOGLE_APPLICATION_CREDENTIALS = 'Hasdrubal.txt';
+  expect(() => writeGoogleCredentials()).toThrow();
+});
+
+test('writeGoogleCredentials writes to file', () => {
+  process.env.GOOGLE_KEY = 'Hamilcar';
+  process.env.GOOGLE_APPLICATION_CREDENTIALS = EXPECTED_FILE;
+
+  writeGoogleCredentials();
+
+  expect(fs.existsSync(EXPECTED_FILE)).toBe(true);
+  expect(fs.readFileSync(EXPECTED_FILE).toString())
+      .toBe(process.env.GOOGLE_KEY);
+});
