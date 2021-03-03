e5bfff83d2dc4db5abe3623099f4228bee798852
Nitin Prasad <nkprasa@gmail.com>
2021-03-03 00:36:31 -0600

    Split up server setup and listen

--- src/server/main.ts
+++ src/server/main.ts
@@ -1,37 +1,6 @@
-import express from 'express';
-import {Server} from 'socket.io';
-import http from 'http';
-import passport from 'passport';
-
-import {setupLogin} from '_server/auth/login';
-import {ROOT} from '_server/util/file_util';
-import {imageRouter} from '_server/routes/images';
-import {contentRouter} from '_server/routes/content';
-import {writeGoogleCredentials} from '_server/util/startup_scripts';
-import {registerBoardRoutes} from '_server/routes/socket/board_socket';
+import {server} from '_server/server';
 
 const PORT = process.env.PORT;
-
-writeGoogleCredentials();
-const app = express();
-const server = http.createServer(app);
-const ioServer = new Server(server);
-registerBoardRoutes(ioServer);
-setupLogin(app, passport);
-app.use(express.static('public'));
-
-// Login routes
-app.get('/', (_req, res) => {
-  res.sendFile('public/templates/login.html', {root: ROOT});
-});
-
-app.post(
-    '/',
-    passport.authenticate(
-        'local', {successRedirect: '/gameBoard', failureRedirect: '/'}));
-
-app.use('/', imageRouter);
-app.use('/', contentRouter);
 
 server.listen(PORT, () => {
   console.log(`server is listening on ${PORT}`);
---
+++ src/server/server.ts
@@ -0,0 +1,34 @@
+/* istanbul ignore file */
+import express from 'express';
+import {Server} from 'socket.io';
+import http from 'http';
+import passport from 'passport';
+
+import {setupLogin} from '_server/auth/login';
+import {ROOT} from '_server/util/file_util';
+import {imageRouter} from '_server/routes/images';
+import {contentRouter} from '_server/routes/content';
+import {writeGoogleCredentials} from '_server/util/startup_scripts';
+import {registerBoardRoutes} from '_server/routes/socket/board_socket';
+
+
+writeGoogleCredentials();
+const app = express();
+export const server = http.createServer(app);
+const ioServer = new Server(server);
+registerBoardRoutes(ioServer);
+setupLogin(app, passport);
+app.use(express.static('public'));
+
+// Login routes
+app.get('/', (_req, res) => {
+  res.sendFile('public/templates/login.html', {root: ROOT});
+});
+
+app.post(
+    '/',
+    passport.authenticate(
+        'local', {successRedirect: '/gameBoard', failureRedirect: '/'}));
+
+app.use('/', imageRouter);
+app.use('/', contentRouter);
