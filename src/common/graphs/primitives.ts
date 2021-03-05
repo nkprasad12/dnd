061c25dc475e023132f6b3f9b8d1fe69dbde371c
Nitin Prasad <nkprasa@gmail.com>
2021-03-04 19:07:39 -0600

    Implement autocompleter

---
+++ src/common/graphs/primitives.ts
@@ -0,0 +1,36 @@
+/** Represents a basic undirected graph node. */
+export class GraphNode<T> {
+  readonly value: T;
+  readonly neighbors: Array<GraphNode<T>> = [];
+
+  constructor(value: T) {
+    this.value = value;
+  }
+
+  /**
+   * Returns the first neighbor with a value that matches the target value.
+   * Returns undefined if no neighbors have the target value.
+   */
+  getChild(searchValue: T): GraphNode<T>|undefined {
+    for (const neighbor of this.neighbors) {
+      if (neighbor.value === searchValue) {
+        return neighbor;
+      }
+    }
+    return undefined;
+  }
+
+  /** Adds the input as a neighbor of this node. */
+  addNeighbor(neighbor: GraphNode<T>): void {
+    this.neighbors.push(neighbor);
+  }
+
+  /**
+   * Creates a node with the given value, adds it as a neighbor, and returns it.
+   */
+  addValueAsNeighbor(value: T): GraphNode<T> {
+    const result = new GraphNode(value);
+    this.addNeighbor(result);
+    return result;
+  }
+}
