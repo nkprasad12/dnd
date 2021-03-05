9693b4f737492ae2e60d222f892eafcdace25a34
Nitin Prasad <nkprasa@gmail.com>
2021-03-04 23:00:24 -0600

    Use command parser in command resolver

--- src/server/chat/chat_resolver.ts
+++ src/server/chat/chat_resolver.ts
@@ -1,4 +1,6 @@
 import {ChatMessage} from '_common/chat/chat_model';
+import {checkDefined} from '_common/preconditions';
+import {CommandType, processCommand} from '_server/chat/command_parser';
 
 
 export type ResolvedCommand = Promise<ChatMessage|undefined>;
@@ -7,22 +9,35 @@ export class CommandResolver {
   constructor() {}
 
   async handleCommand(inputCommand: string): ResolvedCommand {
-    const body = inputCommand.trim();
-    if (!body.startsWith('!roll ')) {
-      return undefined;
+    const result = processCommand(inputCommand);
+    if (result.error) {
+      if (result.error.possibleTypes.length === 0) {
+        // If it's not parsable as a command, it's probably a regular chat.
+        return undefined;
+      }
+      const header = 'Got ambiguous command ' + result.error.commandAttempt;
+      const body =
+          'Could be either ' + JSON.stringify(result.error.possibleTypes);
+      return {header: header, body: body};
     }
-    const command = body.split(' ')[1];
-    const diceParts = command.split('d');
+    const command = checkDefined(result.command);
+    if (command.command !== CommandType.Roll) {
+      return {
+        header: inputCommand,
+        body: 'Not supported yet - coming soon!'};
+    }
+
+    const diceParts = command.query.split('d');
     if (diceParts.length < 2) {
-      return rollErrorMessage(body);
+      return rollErrorMessage(command.query);
     }
     const numDice = parseInt(diceParts[0]);
     if (numDice === undefined || numDice < 1) {
-      return rollErrorMessage(body);
+      return rollErrorMessage(command.query);
     }
     const numSides = parseInt(diceParts[1]);
     if (numSides === undefined || numSides < 1) {
-      return rollErrorMessage(body);
+      return rollErrorMessage(command.query);
     }
     return rollMessage(numDice, numSides);
   }
@@ -51,7 +66,7 @@ function getRandomIntInclusive(min: number, max: number): number {
 }
 
 function rollErrorMessage(input: string): ChatMessage {
-  const header = `${input} is not a valid input.`;
+  const header = `${input} is not a valid roll input.`;
   const usage = 'Example: !roll 2d20 to roll 2 dice with 20 sides.';
   return {header: header, body: usage};
 }
