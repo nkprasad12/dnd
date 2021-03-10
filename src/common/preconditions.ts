8b808876e8d354ec8ad9d7df19ec5ad98dc9b65b
Nitin Prasad <nkprasa@gmail.com>
2021-03-10 01:34:12 -0600

    Add precondition checkState

--- src/common/preconditions.ts
+++ src/common/preconditions.ts
@@ -1,3 +1,9 @@
+export function checkState(condition: boolean, message: string): void {
+  if (!condition) {
+    throw new Error(message);
+  }
+}
+
 export function checkDefined<T>(input: T | undefined | null, name?: string): T {
   if (input === undefined || input === null) {
     if (name === undefined) {
