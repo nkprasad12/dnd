d6750e60b62530c2dd9f4b34a3a5843c3e1e6a45
Nitin Prasad <nkprasa@gmail.com>
2021-04-06 21:19:47 -0500

    Remove obsolete boardTools route

--- src/server/routes/content.ts
+++ src/server/routes/content.ts
@@ -10,10 +10,6 @@ export const contentRouter = express.Router();
 contentRouter.use(ensureLoggedIn('/'));
 contentRouter.use(expressStaticGzip('genfiles_static', {index: false}));
 
-contentRouter.get('/boardTools', (_req, res) => {
-  res.sendFile('genfiles_static/board_tools.html', {root: ROOT});
-});
-
 contentRouter.get('/gameBoard', (_req, res) => {
   res.sendFile('genfiles_static/game_board.html', {root: ROOT});
 });
