d233131cd05577e01bf84f1c3f2ac2c160c69a38
Nitin Prasad <nkprasa@gmail.com>
2021-04-07 23:05:47 -0500

    Add utility for extracting sheetId from RL

---
+++ src/common/character_sheets/utils.ts
@@ -0,0 +1,15 @@
+const SHEET_ID_PREFIX = 'spreadsheets/d/';
+
+export namespace Sheets {
+  /**
+   * Extracts a sheet id from a Google Sheet URL.
+   *
+   * Returns `null` if no sheet id could be found in the input URL.
+   */
+  export function idFromUrl(url: string): string | null {
+    if (url.indexOf(SHEET_ID_PREFIX) === -1) {
+      return null;
+    }
+    return url.split(SHEET_ID_PREFIX)[1].split('/')[0];
+  }
+}
